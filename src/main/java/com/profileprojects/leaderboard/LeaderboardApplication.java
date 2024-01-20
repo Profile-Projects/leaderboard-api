2bfd6e0297bc22223024bc3b5959590e817b702a
santhosh <santhoshg1990@gmail.com>
2024-01-20 15:17:50 +0530

    initail

---
+++ src/main/java/com/profileprojects/leaderboard/LeaderboardApplication.java
@@ -0,0 +1,13 @@
+package com.profileprojects.leaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderboardApplication.class, args);
+	}
+
+}
